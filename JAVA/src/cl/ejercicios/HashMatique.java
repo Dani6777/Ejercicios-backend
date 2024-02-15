32706b72c2b23673c62d8ed34cc25757d2ba8884
DANIEL MORALES <daniel.morales@outlook.cl>
2024-02-15 14:05:31 -0300

    Hash MATIQUE

---
+++ JAVA/src/cl/ejercicios/HashMatique.java
@@ -0,0 +1,27 @@
+package cl.ejercicios;
+
+import java.util.HashMap;
+
+public class HashMatique {
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> trackList = new HashMap<String, String>();
+
+        trackList.put("Molinos de viento", "Si acaso tú no ves más allá de tu nariz");
+        trackList.put("Fiesta pagana", "No me digas que no sientes nada al escuchar esta canción");
+        trackList.put("El que quiera entender que entienda",
+                "El que quiera entender que entienda, el que quiera seguir que siga");
+        trackList.put("La costa del silencio", "En la costa del silencio, donde solo habita el miedo");
+
+        String cancion = trackList.get("Fiesta pagana");
+
+        System.out.println("La letra de la canción es: " + cancion);
+
+        for (String key : trackList.keySet()) {
+            System.out.println("Track: " + key);
+            System.out.println("Lyrics: " + trackList.get(key));
+        }
+    }
+
+}

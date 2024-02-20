329233017a416d58a43eb02e568ff729f80465e5
Daniel Morales Arias <daniel.morales23@outlook.clm>
2024-02-20 19:36:27 -0300

    add doc

--- JAVA/src/cl/java/HashMapTesting.java
+++ JAVA/src/cl/java/HashMapTesting.java
@@ -5,6 +5,13 @@ import java.util.Set;
 
 public class HashMapTesting {
 
+    /**
+     * The main method is the entry point of the program.
+     * It demonstrates the usage of a HashMap to store and retrieve user
+     * information.
+     *
+     * @param args The command-line arguments passed to the program.
+     */
     public static void main(String[] args) {
 
         HashMap<String, String> userMap = new HashMap<String, String>();
--- JAVA/src/cl/poo/Poo/src/main/java/cl/daniel/poo/Auto.java
+++ JAVA/src/cl/poo/Poo/src/main/java/cl/daniel/poo/Auto.java
@@ -1,15 +1,14 @@
 
 package cl.daniel.poo;
 
-
+/**
+ * Represents a car.
+ */
 public class Auto {
-    
+
     String patente;
     String color;
     int kms;
     boolean conAire;
-    
-    
-    
-    
+
 }

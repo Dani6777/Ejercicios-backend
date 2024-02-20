8a85701d5b12e96c44b804b24f3899a1d8224d2b
Daniel Morales Arias <daniel.morales23@outlook.clm>
2024-02-20 19:29:11 -0300

    add doc

--- JAVA/src/cl/poo/Poo/src/main/java/cl/daniel/poo/Auto.java
+++ JAVA/src/cl/poo/Poo/src/main/java/cl/daniel/poo/Auto.java
@@ -1,15 +1,29 @@
 
 package cl.daniel.poo;
 
-
+/**
+ * The Auto class represents a car.
+ */
 public class Auto {
-    
+
+    /**
+     * The license plate of the car.
+     */
     String patente;
+
+    /**
+     * The color of the car.
+     */
     String color;
+
+    /**
+     * The number of kilometers the car has traveled.
+     */
     int kms;
+
+    /**
+     * Indicates whether the car has air conditioning or not.
+     */
     boolean conAire;
-    
-    
-    
-    
+
 }

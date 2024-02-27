e0dac84db574d62d9be3d6edcbe14616c9bbab91
Daniel Morales <dmorales.vrgroup@bicevida.cl>
2024-02-27 11:11:50 -0300

    FIX STRUCTURED PROJECT

---
+++ Jav/src/main/java/cl/sence/daniel/datos/Arreglos.java
@@ -0,0 +1,18 @@
+package cl.sence.daniel.datos;
+
+public class Arreglos {
+
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
+
+        int[] numbers = { 2, 3, 4, 5 };
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}

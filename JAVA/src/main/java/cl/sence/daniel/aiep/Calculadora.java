7af43ae390dd58b7f73ed9faaebc50563998b3f6
Daniel Morales <daniel.morales23@outlook.cl>
2024-04-10 21:16:54 -0400

    agregando clases aiep miercoles 10 abril

---
+++ JAVA/src/main/java/cl/sence/daniel/aiep/Calculadora.java
@@ -0,0 +1,32 @@
+package cl.sence.daniel.aiep;
+
+public class Calculadora {
+
+    public  static void main(String[] args) {
+        System.out.println("Suma: " + sumar(5, 3));
+        System.out.println("Resta: " + restar(5, 3));
+        System.out.println("Multiplicación: " + multiplicar(5, 3));
+        System.out.println("División: " + dividir(5, 3));
+    }
+
+    //int es un tipo de dato de tipo entero
+    public static int sumar(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static int restar(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public static int multiplicar(int num1, int num2){
+        return num1 * num2;
+    }
+    public static int dividir(int num1, int num2){
+        if (num2 == 0){
+            System.out.println("No se puede dividir por 0");
+            return 0;
+        }else {
+            return num1 / num2;
+        }
+    }
+}
